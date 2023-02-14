f290f53b9049bfed93f79e0e32b0ecb390c56dad
jojo1234526 <josephgonzalez11051998@gmail.com>
2023-02-14 21:05:14 +0000

    project done

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Account;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,24 +10,74 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    public class Account {
+        private int accountId;
+        public int getAccountId() {
+            return accountId;
+        }
+        public void setAccountId(int accountId) {
+            this.accountId = accountId;
+        }
+        private String username;
+        public String getUsername() {
+            return username;
+        }
+        public void setUsername(String username) {
+            this.username = username;
+        }
+        private String password;
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+     
+        // Getters and setters for the properties
+        // ...
+     }
+
+    public Account registerAccount(@RequestBody Account account) {
+        // Validate the input and register the new account
+        // ...
+        return account;
+     }
+
+     
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
+    
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
-
+        app.post("/register", this::registerHandler);
+        app.post("/login", this::loginHandler);
+        app.post("/messages", this::createMessageHandler);
         return app;
     }
 
+    
     /**
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+
+    private void registerHandler(Context context1) {
+    }
+
+
+    private void loginHandler(Context context1) {
+    }
+
+
+    private void createMessageHandler(Context context1) {
     }
 
 
